123e23d5abc8b47f1665ae3c39f0f1bafa8d5418
Juha-Matti Santala <juhamattisantala@gmail.com>
2018-03-02 16:53:04 +0200

    Add subscription option to registration page

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -11,21 +11,28 @@
     <div class="loginform">
       <input type="text" name="email" placeholder="Sähköposti" v-model="email" >
       <button class="button button-block" @click="subscribe">Lisää listalle</button>
+      <div class="success" v-if="msg"> {{ msg }} </div>
     </div>
   </div>
 </template>
 
 <script>
+import db from '@/firebase.js'
+
 export default {
   name: 'register',
   data () {
     return {
-      email: ''
+      email: '',
+      msg: ''
     }
   },
   methods: {
     subscribe () {
-      // TODO: Save email to Firebase
+      let ref = db.ref(`/subscribed`).push()
+      ref.set(this.email).then(() => {
+        this.msg = 'Sähköpostisi on lisätty listalle! Mukavaa päivänjatkoa'
+      })
     }
   }
 }
